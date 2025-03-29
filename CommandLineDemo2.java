2f54be7f422987b211ddd375b09050fa33efcdad
Satwika-27 <bsatwika27@gmail.com>
2025-03-29 12:27:58 +0530

    Add files via upload

---
+++ CommandLineDemo2.java
@@ -0,0 +1,14 @@
+class CommandLineDemo2
+{
+public static void main(String args[])
+{
+String n1,n2,n3;
+n1=args[0];
+n2=args[1];
+n3=args[2];
+System.out.println("Name:"+n1);
+System.out.println("Rollno:"+n2);
+System.out.println("Department name:"+n3);
+}
+}
+
