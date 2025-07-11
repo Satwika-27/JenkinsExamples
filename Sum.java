bb24d0210eb0381173b7590736ba7d2410daf0ac
Satwika-27 <bsatwika27@gmail.com>
2025-07-11 12:59:02 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+class Sum{
+public static void main(String args[])
+{
+int sum=0;
+int avg=0;
+for(int i=1;i<=10;i++)
+  sum=sum+i;
+  avg=sum/10;
+System.out.println(sum);
+System.out.println(avg);
+}
+}
+
